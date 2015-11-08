e96548ccb7a712271edcd17d4b84c7bf4f37b11c
Notboby <RobinFabre@macbook-pro-de-robin.home>
2015-11-08 19:04:15 +0100

    iOS Single View Unit Converter App

--- UnitConverter/ViewController.m
+++ UnitConverter/ViewController.m
@@ -8,11 +8,83 @@
 
 #import "ViewController.h"
 
+//the three functions to convert
+
+//convert metre to inch
+double convertUnitOneToUnitTwo(double unitOneValue){
+    double unitTwoValue;
+    
+    unitTwoValue = (unitOneValue*100)/2.54;
+    return unitTwoValue;
+}
+//convert metre to Foot
+
+double convertUnitOneToUnitThree(double unitOneValue){
+    double unitThreeValue;
+    
+    unitThreeValue = unitOneValue*3.2809;
+    return unitThreeValue;
+}
+//convert metre to Yard
+
+double convertUnitOneToUnitFour(double unitOneValue){
+    double unitFourValue;
+    
+    unitFourValue = unitOneValue/0.9144;
+    return unitFourValue;
+}
+
+
 @interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet UITextField *inputfield;
+
+@property (weak, nonatomic) IBOutlet UISegmentedControl *segmentControler;
+
+
+@property (weak, nonatomic) IBOutlet UILabel *outputField;
+
 
 @end
 
 @implementation ViewController
+
+- (IBAction)updateButton:(id)sender {
+
+    NSMutableString *buf = [NSMutableString new];
+    
+    double userInput = [self.inputfield.text doubleValue];
+    
+//the three conditions
+//if inch selected
+    
+    if(self.segmentControler.selectedSegmentIndex == 0){
+        
+        double unitTwoValue = convertUnitOneToUnitTwo(userInput);
+        
+        [buf appendString: [@(unitTwoValue) stringValue]];
+
+    }
+//if foot selected
+
+    else if(self.segmentControler.selectedSegmentIndex == 1){
+        double unitThreeValue = convertUnitOneToUnitThree(userInput);
+        
+        [buf appendString: [@(unitThreeValue) stringValue]];
+    }
+//if yard selected
+
+    else {
+        double unitFourValue = convertUnitOneToUnitFour(userInput);
+        
+        [buf appendString: [@(unitFourValue) stringValue]];
+
+    }
+    
+    
+    self.outputField.text = buf;
+
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
